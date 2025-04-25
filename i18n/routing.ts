50cc91d521a871f62b6ffbab928d71f481e53003
“afredzhou” <afred.zhou.cn@gmail.com>
2025-04-25 23:40:54 +0800

    增加link

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -5,13 +5,10 @@ export type Locale = 'en' | 'zh';
 export const routing = defineRouting({
   // Supported locales
   locales: ['en', 'zh'],
-
   // Default locale when no match
   defaultLocale: 'en',
-
   // Use prefix-based routing only for non-default locales
   localePrefix: 'as-needed',
-
   pathnames: {
     '/': '/',
     '/pathnames': {
