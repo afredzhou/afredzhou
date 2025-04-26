ada86fbe9280e8fc99fe69656b8c4d235bb88560
“afredzhou” <afred.zhou.cn@gmail.com>
2025-04-26 16:07:29 +0800

    add

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,5 @@ export default createMiddleware(routing);
 export const config = {
   // Match only internationalized pathnames
   // Skip internal paths (_next) and static assets (e.g., images, favicon)
-  matcher: ['/((?!api|_next|_vercel|studio|admin|.*\\..*).*)'],
-
+ matcher: '/((?!api|trpc|_next|_vercel|studio|.*\\..*).*)'
 };
