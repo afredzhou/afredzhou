0c31b84f63cbb89bfcd8cf290257c0f2ca4468b0
“afredzhou” <afred.zhou.cn@gmail.com>
2025-04-26 17:30:07 +0800

    6666

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,13 +1,9 @@
 import {defineRouting} from 'next-intl/routing';
 
-export type Locale = 'en' | 'zh';
-
 export const routing = defineRouting({
-  // Supported locales
   locales: ['en', 'zh'],
-  // Default locale when no match
   defaultLocale: 'en',
-  // Use prefix-based routing only for non-default locales
+  localePrefix: 'as-needed',
   pathnames: {
     '/': '/',
     '/pathnames': {
